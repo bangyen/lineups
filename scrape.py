8053ecd0019e2ff3d2063acc7d679ffd6d854bf5
Bangyen Pham <bangyen99@gmail.com>
2023-11-28 20:03:19 -0600

    added genre lookup

--- scrape.py
+++ scrape.py
@@ -1,7 +1,13 @@
-from bs4 import BeautifulSoup
+from   spotipy.oauth2  import SpotifyClientCredentials
+from   bs4             import BeautifulSoup
+from   multiprocessing import Pool
+import spotipy
 import sys
 import os
-import re
+
+spotify = spotipy.Spotify(
+    auth_manager=SpotifyClientCredentials()
+)
 
 def parse(html):
     soup = BeautifulSoup(html, 'html.parser')
@@ -12,8 +18,26 @@ def parse(html):
     span = soup.find_all('span', class_='nxucXc CxwsZe')
     return [s.string for s in span]
 
+
+def search(name):
+    results = spotify.search(
+        q='artist:' + name,
+        type='artist'
+    )
+
+    items = results['artists']['items']
+
+    if len(items) == 0:
+        return
+
+    artist = items[0]
+    return artist['name'], artist['genres']
+
+
 if __name__ == '__main__':
     args = sys.argv
+    pool = Pool()
+    res  = []
 
     if len(args) == 1:
         exit('Please supply a file.')
@@ -23,6 +47,14 @@ if __name__ == '__main__':
 
     with open(args[1], encoding='utf-8') as file:
         html = file.read()
-        print(parse(html))
+        res  = pool.map(search, html)
+
+        for n in html:
+            proc = pool.apply_async(search, [n])
+            res.append(proc)
+
+        for p in res:
+            t = p.get(timeout=10)
+            print(t)
 
         file.close()
