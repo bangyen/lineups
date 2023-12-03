9b972946cac13952eb303ceff9f342475f3ac94e
bangyen <bangyen@northwestern.edu>
2023-12-03 15:21:30 -0600

    fixed imports

--- auto.sh
+++ auto.sh
@@ -5,7 +5,7 @@ file=${list[0]}
 
 if [[ $file == *\.html ]]; then
     echo "=== Current file: $file ==="
-    python3 src/collect.py "$file"
+    python3 -m src.collect "$file"
     mv "$file" "data/done"
 
     num=$(($(ls -1 backup | wc -l) + 1))
