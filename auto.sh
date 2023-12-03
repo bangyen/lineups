f833ac7c1a99b14963a4ca849520234d18bab37f
bangyen <bangyen@northwestern.edu>
2023-12-03 15:52:43 -0600

    added error handling

--- auto.sh
+++ auto.sh
@@ -6,10 +6,15 @@ file=${list[0]}
 if [[ $file == *\.html ]]; then
     echo "=== Current file: $file ==="
     python3 -m src.collect "$file"
-    mv "$file" "data/done"
 
-    num=$(($(ls -1 backup | wc -l) + 1))
-    cp json.zlib backup/backup_$num.zlib
-    echo "=== Created backup_$num.zlib ==="
+    if [ $? -eq 0 ]; then
+        num=$(($(ls -1 backup | wc -l) + 1))
+        echo "=== Creating backup_$num.zlib ==="
+
+        cp json.zlib backup/backup_$num.zlib
+        mv "$file" "data/done"
+    fi
+else
+    echo "=== Invalid file: $file ==="
 fi
 
