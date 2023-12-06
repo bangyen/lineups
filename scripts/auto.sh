26f347fc6846e53d8f52f1749b36e532e40c9afa
bangyen <bangyen@northwestern.edu>
2023-12-06 01:39:43 -0600

    added scripts folder

--- scripts/auto.sh
+++ scripts/auto.sh
@@ -2,7 +2,7 @@
 
 for file in data/*.html; do
     echo "=== Current file: $file ==="
-    python3 -m main "$file"
+    python3 -m scripts.main "$file"
 
     if [ $? -eq 0 ]; then
         num=$(($(ls -1 backup | wc -l) + 1))
