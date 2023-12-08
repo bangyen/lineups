9a27756f424ef108faf82762c11e907bf2da1ac7
bangyen <bangyen@northwestern.edu>
2023-12-08 00:36:21 -0600

    added data entry func

--- src/edit.py
+++ src/edit.py
@@ -2,12 +2,52 @@ import src.generate as generate
 import src.collect  as collect
 import os
 
+def interact(tables):
+    fest = artist = True
+    year = bill   = ''
+    res  = []
+
+    while fest:
+        fest = input('Festival: ')
+
+        if fest not in tables.fests:
+            continue
+
+        while not year.isdigit():
+            year = input('Year: ')
+
+        while artist:
+            artist = input('Artist: ')
+            status = input('Headliner (y/n): ')
+
+            if not status:
+                break
+
+            if status == '!redo':
+                continue
+
+            if status and status[0] == 'y':
+                bill = 'Headliner'
+            else:
+                bill = 'Subheadliner'
+
+            row = (artist, bill, fest, year)
+            res.append(row)
+
+    if res:
+        billing(res, tables)
+
+
 def billing(lines, tables):
     opts  = ('Headliner', 'Subheadliner')
     names = []
 
     for row in lines:
-        args = row.rsplit(maxsplit=3)
+        if isinstance(row, str):
+            args = row.rsplit(maxsplit=3)
+        else:
+            args = row
+
         a, b, f, y = args
         y = int(y)
 
@@ -47,4 +87,5 @@ if __name__ == '__main__':
             lines = file.readlines()
             billing(lines, tables)
 
+    interact(tables)
     generate.dumps(tables, name)
