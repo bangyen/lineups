98258bc9fc7e2525bfe02cd29cdcf4008b6f725c
Bangyen Pham <bangyen99@gmail.com>
2024-06-19 03:13:00 -0500

    added convert tests

--- tests/test_lang.py
+++ tests/test_lang.py
@@ -1,5 +1,6 @@
 from src import lang
 import unittest
+import re
 
 class TestLanguageFunctions(unittest.TestCase):
     def test_match(self):
@@ -32,6 +33,33 @@ class TestLanguageFunctions(unittest.TestCase):
         self.assertIsNone(check('bar baz'    ))
         self.assertIsNone(check('baz foo'    ))
         self.assertIsNone(check('bar foo baz'))
+
+    def test_convert(self):
+        conv = lang.conv
+        innr = {'foo': 'baz'}
+        outr = {'bar': innr}
+
+        toks = ['or', 'key', '=~', 'foo']
+        func = [conv(t) for t in toks]
+
+        for k in range(len(toks)):
+            with self.subTest(i=toks[k]):
+                self.assertTrue(callable(func[k]))
+
+        self.assertTrue(conv('True'))
+        self.assertTrue(func[0](True, False))
+
+        self.assertEqual(conv('""'),   '')
+        self.assertEqual(conv('100'), 100)
+        self.assertEqual(func[1]({},   'bar'), 'bar')
+        self.assertEqual(func[3](outr, 'bar'), 'baz')
+
+        self.assertTrue(
+            isinstance(
+                func[2]('bar', 'b'),
+                re.Match
+        ))
+
 
 if __name__ == '__main__':
     unittest.main()
